368d74ec03f3f0769d626ffd33411de831615c59
Sambit Kumar Jena <159466319+Sambit777@users.noreply.github.com>
2024-03-08 21:48:47 +0530

    Add files via upload

---
+++ Array Questions/array1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   
+
+    int a;
+    cin >> a;
+
+    for (int i = 0; i < a; i++) {
+        int n;
+        cin >> n;
+
+        int arr[n];
+        for (int j = 0; j < n; j++) {
+            cin >> arr[j];
+        }
+
+        // Perform array reversal
+        for (int j = 0; j < n / 2; j++) {
+            swap(arr[j], arr[n - j - 1]);
+        }
+
+        // Print the array
+        for (int j = 0; j < n; j++) {
+            cout << arr[j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
